dfe39652fe686229601fbefa80d57f44bc64cf0b
Aditya Singh <adityasingh40675@gmail.com>
2024-07-06 10:20:11 +0530

    added roles and userRoles through tables

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+const { User, Role} = require('./models/index');
+
 const prepareAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api', apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log('Server started at', PORT);
+
+        const u1 = await User.findByPk(2);
+        const r1 = await Role.findByPk(1);
+        u1.addRole(r1);
     })
 }
 
